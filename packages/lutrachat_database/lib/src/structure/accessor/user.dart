8a8e886521e41f009008b34591426214da40b849
kasefuchs <kasefuchs@protonmail.com>
2024-06-18 22:36:25 +0300

    :construction: Add user table accessor

---
+++ packages/lutrachat_database/lib/src/structure/accessor/user.dart
@@ -0,0 +1,41 @@
+import 'package:drift/drift.dart';
+import 'package:foxid/foxid.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../service/database.dart';
+import '../converter/foxid.dart';
+import '../table/user.dart';
+
+part 'user.g.dart';
+
+/// Data accessor for the user table.
+@lazySingleton
+@DriftAccessor(tables: [UserTable])
+final class UserAccessor extends DatabaseAccessor<DatabaseService>
+    with _$UserAccessorMixin {
+  UserAccessor(super.attachedDatabase);
+
+  /// Find a user by their ID.
+  Future<UserTableData?> findById(FOxID id) async {
+    final idSql = FOxIDConverter.instance.toSql(id);
+    final query = select(userTable)..where((entity) => entity.id.equals(idSql));
+
+    return await query.getSingleOrNull();
+  }
+
+  /// Find a user by their name.
+  Future<UserTableData?> findByName(String username) async {
+    final query = select(userTable)
+      ..where((entity) => entity.name.equals(username));
+
+    return await query.getSingleOrNull();
+  }
+
+  /// Find a user by their email address.
+  Future<UserTableData?> findByEmail(String email) async {
+    final query = select(userTable)
+      ..where((entity) => entity.email.equals(email));
+
+    return await query.getSingleOrNull();
+  }
+}
